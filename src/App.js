3da8df2677bb230f2a234076eb37f722c1ec5a47
g0vind-00 <87952784+g0vind-00@users.noreply.github.com>
2021-07-26 18:56:21 +0530

    step1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import HeaderNav from "./containers/HeaderNav/HeaderNav";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderNav />
+    </div>
+  );
+}
+
+export default App;
