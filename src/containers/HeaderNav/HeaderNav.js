7fb2a415b098c87cd1e1e1cedffa462804cb0a2b
g0vind-00 <87952784+g0vind-00@users.noreply.github.com>
2021-07-26 23:32:59 +0530

    step1 completed

--- src/containers/HeaderNav/HeaderNav.js
+++ src/containers/HeaderNav/HeaderNav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Menu } from "semantic-ui-react";
+import { Form, Icon, Image, Input, Menu } from "semantic-ui-react";
 import "./HeaderNav.scss";
 import logo from "../../assets/images/logo.jpg";
 
@@ -10,7 +10,35 @@ function HeaderNav() {
         <Menu.Item header className="logo">
           <Image src={logo} size="tiny" />
         </Menu.Item>
-        <Menu.Menu className="nav-container">{/* TODO */}</Menu.Menu>
+        <Menu.Menu className="nav-container">
+          <Menu.Item className="search-input">
+            <Form>
+              <Form.Field>
+                <Input placeholder="Search" size="small" action="Go" />
+              </Form.Field>
+            </Form>
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Icon className="header-icon" name="video camera" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="grid layout" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="chat" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="alarm" size="large" />
+            </Menu.Item>
+            <Menu.Item name="avatar">
+              <Image
+                src="https://cdn.pixabay.com/photo/2019/08/11/18/59/icon-4399701_960_720.png"
+                avatar
+              />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Menu>
       </Menu>
     </div>
   );
